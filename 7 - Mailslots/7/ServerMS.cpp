ca3ba2c723f0da90162f4778c05d1b57a948c5a8
Klepaski <43060010+klepaski@users.noreply.github.com>
2020-01-24 17:25:30 +0300

    Add files via upload

---
+++ 7 - Mailslots/7/ServerMS.cpp
@@ -0,0 +1,111 @@
+﻿#include "pch.h"
+#include <string>
+#include "iostream"
+#include "windows.h"
+#include "time.h"
+
+using namespace std;
+
+string GetErrorMail(int code)
+{
+	string msgText = "";
+
+	switch (code)
+	{
+	case WSAEINTR: msgText = "WSAEINTR"; break;
+	case WSAEACCES:	msgText = "WSAEACCES"; break;
+	case WSAEFAULT:	msgText = "WSAEFAULT"; break;
+	default: msgText = "Error";
+	};
+
+	return msgText;
+}
+
+string SetErrorMail(string msgText, int code)
+{
+	return msgText + GetErrorMail(code);
+}
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+
+	cout << "ServerMS" << endl << endl;
+
+	try
+	{
+		HANDLE hMS;
+		double t1, t2;
+		int i = 0;
+
+		//созд ПЯ
+		///							симв имя		макс длина сообщ	t ожид		атриб безоп
+		if ((hMS = CreateMailslot(L"\\\\.\\mailslot\\BOX", 300, MAILSLOT_WAIT_FOREVER, NULL)) == INVALID_HANDLE_VALUE)
+			throw SetErrorMail("CreateMailslot", GetLastError());
+
+		char rbuf[50];
+		DWORD rms;
+		do
+		{
+			i++;
+			//чит д-е из ПЯ (м. т. сервер)
+			if (!ReadFile(hMS, rbuf, sizeof(rbuf), &rms, NULL))
+				throw SetErrorMail("ReadFile", GetLastError());
+
+			if (i == 1)
+				t1 = clock();
+
+			cout << rbuf << endl;
+		} while (strcmp(rbuf, " ") != 0);
+
+		t2 = clock();
+
+		cout << "Время передачи: " << (t2 - t1) / 1000 << " сек." << endl;
+		cout << "Количество сообщений: " << i - 1 << endl << endl;
+
+		if (!CloseHandle(hMS))
+			throw "Error: CloseHandle";
+
+		system("pause");
+
+		HANDLE hms1;
+
+		//откр ПЯ
+		//hms1 = CreateFile(L"\\\\ASUS\\mailslot\\zxc", GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
+		//char wbuf[50] = "Jumanji";
+		//DWORD wms;
+		//for (int i = 0; i < 1000; i++)
+		//{
+		//	char q[6];
+		//	sprintf_s(q, "%d", i + 1);
+		//	char buf[sizeof(wbuf) + sizeof(q)];
+		//	strcpy_s(buf, wbuf);
+		//	strcat_s(buf, q);
+		//	//пис д-е в ПЯ
+		//	if (!WriteFile(hms1, buf, sizeof(wbuf), &wms, NULL))
+		//		throw SetErrorMail("WriteFile: ", GetLastError());
+		//	cout << "Message " << i + 1 << endl;
+		//}
+		//if (!CloseHandle(hms1))
+		//	throw "Error: CloseHandle";
+		//system("pause");
+	}
+	catch (string e)
+	{
+		cout << e << endl;
+	}
+
+	return 0;
+}
+
+
+///	sH = createMailslot			cH = createFile		//подсоед к ПЯ
+///	char rbuf[...]				char wbuf[...]
+///								WriteFile
+///	ReadFile
+///	CloseHandle					CloseHandle			//уничт
+
+///чтобы в 2 стороны - у каждого процесса свой ПЯ
+///лок формат:	\\ . \ mailslot \ имя_пя
+///сет формат:	\\ servname \ mailslot \ имя_пя
+///домен форм:	\\ domain \ mailslot \ имя_пя
---
+++ 7 - Mailslots/ClientMS/ClientMS.cpp
@@ -0,0 +1,106 @@
+﻿#include "pch.h"
+#include <string>
+#include "iostream"
+#include "windows.h"///его достаточно для MS
+#include "time.h"
+
+using namespace std;
+
+string GetErrorMail(int code)
+{
+	string msgText = "";
+
+	switch (code)
+	{
+	case WSAEINTR: msgText = "WSAEINTR"; break;
+	case WSAEACCES:	msgText = "WSAEACCES"; break;
+	case WSAEFAULT:	msgText = "WSAEFAULT"; break;
+	default: msgText = "Error";
+	};
+
+	return msgText;
+}
+
+string SetErrorMail(string msgText, int code)
+{
+	return msgText + GetErrorMail(code);
+}
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+
+	cout << "ClientMS" << endl << endl;
+
+	try
+	{
+		HANDLE hMS;
+		double t1, t2;
+		//подсоед к ПЯ
+		///						симв имя пя				чт/зап			реж совм исп				  атр безоп  флаг откр-я	флаги и атр
+		if ((hMS = CreateFile(L"\\\\.\\mailslot\\BOX", GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL)) == INVALID_HANDLE_VALUE)
+			throw SetErrorMail("CreateFile: ", GetLastError());
+
+		char wbuf[50] = "Guka Zaka ";
+		DWORD wms;
+
+		t1 = clock();
+
+		for (int i = 0; i < 1000; i++)
+		{
+			char q[6];
+			sprintf_s(q, "%d", i + 1);
+			char buf[sizeof(wbuf) + sizeof(q)];
+			strcpy_s(buf, wbuf);
+			strcat_s(buf, q);
+			//писать в ПЯ
+			///
+			if (!WriteFile(hMS, buf, sizeof(wbuf), &wms, NULL))
+				throw SetErrorMail("WriteFile: ", GetLastError());
+
+			cout << "Message " << i + 1 << endl;
+		}
+
+		t2 = clock();
+
+		if (!WriteFile(hMS, " ", sizeof(" "), &wms, NULL))
+			throw SetErrorMail("WriteFile: ", GetLastError());
+
+		if (!CloseHandle(hMS))
+			throw "Error: CloseHandle";
+
+		cout << endl << "Время передачи: " << (t2 - t1) / 1000 << " сек." << endl << endl;
+	}
+	catch (string e)
+	{
+		cout << e << endl;
+	}
+	/*
+	HANDLE hMS1;
+	DWORD asd;
+	while (true) {
+		//созд почт ящик
+		hMS1 = CreateMailslot(L"\\\\.\\mailslot\\zxc", 300, MAILSLOT_WAIT_FOREVER, NULL);
+		cout << "mailslot";
+		char buff[50];
+		int j = 0;
+
+
+		do {
+			j++;
+			//чит д-е из почт ящика
+			ReadFile(hMS1, buff, sizeof(buff), &asd, NULL);
+			cout << buff << endl;
+
+		} while (strcmp(buff, " ") != 0);
+
+		CloseHandle(hMS1);
+	}*/
+
+	system("pause");
+	return 0;
+}
+
+
+///CreateFile - нет атр безоп (по умолч) 
+///	=> связь мб уст т. м/процессами запущ от 1 имени с общим паролея
